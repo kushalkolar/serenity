d57b8c93e5292caad4067150f9078cd76d945dc0
kushalkolar <kushalkolar@gmail.com>
2023-03-29 00:13:01 -0400

    get all scanimage metadata, refactor functions

---
+++ matlab/metadata/get_automated_acq_meta.m
@@ -0,0 +1,20 @@
+function rval = get_automated_acq_meta(hsi)
+    % makes some of the acq meta data automatically
+    rval = struct();
+    rval.framerate = hsi.hRoiManager.scanFrameRate;
+    rval.date = datestr(now, "yyyymmdd-HHMMSS");
+    % the scanimage state
+    rval.scanimage_meta = get_scanimage_metadata(hsi);
+    
+    n_channels = max(hsi.hChannels.channelsActive);
+    channel_metadata = cell(n_channels, 1);
+    
+    for i = 1:n_channels
+        channel_metadata{i} = struct();
+        n_rows = hsi.hRoiManager.linesPerFrame;
+        n_cols = hsi.hRoiManager.pixelsPerLine;
+        channel_metadata{i}.shape = [n_rows, n_cols];
+        channel_metadata{i}.dtype = hsi.hScan2D.channelsDataType;
+    end
+    rval.channels = channel_metadata;
+end
---
+++ matlab/metadata/get_scanimage_metadata.m
@@ -0,0 +1,58 @@
+function meta = get_scanimage_metadata(hsi)
+    % get relevant metadata from hSI object
+    meta = struct();
+
+    % the "main" fields directly under hSI
+    main_fields = fieldnames(hsi);
+    for i = 1:numel(main_fields)
+        % keep only native types, not scanimage objects
+        kind = class(hsi.(main_fields{i}));
+
+            % set metadata field only if it's not a scanimage object
+            if ~contains(kind, ".")
+                meta.(main_fields{i}) = hsi.(main_fields{i});
+            end
+    end
+
+    % all other metadata
+    meta_fields = {
+        "hRoiManager",
+        "hBeams",
+        "hMotors",
+        "hChannels",
+        "hShutters",
+        "hDisplay",
+        "hUserFunctions",
+        "hScan2D"
+    };
+
+    % each meta data field
+    for i = 1:numel(meta_fields)
+        s = struct();
+        % get the all the field names under this
+        sub_fields = fieldnames(hsi.(meta_fields{i}));
+
+        for j = 1:numel(sub_fields)
+            % keep only native types, don't keep scanimage objects
+            % scanimage objects contain interfaces to the hardware etc.
+            % does not make sense to serialize hardware objects
+            kind = class(hsi.(meta_fields{i}).(sub_fields{j}));
+
+            % set metadata field only if it's not a scanimage object
+            if ~contains(kind, ".")
+                s.(sub_fields{j}) = hsi.(meta_fields{i}).(sub_fields{j});
+            end
+        end
+
+        % set the meta data field
+        meta.(meta_fields{i}) = s;
+    end
+
+    % remove some non-serializable objects
+    meta.hBeams = rmfield(meta.hBeams, "pzFunction ");
+    meta.hBeams = rmfield(meta.hBeams, "hBeams");
+    
+    % remove shutter data for now 
+    % contains lots of non-serializable hardware objcs
+    meta.hShutters = rmfield(meta.hShutters, "hShutters");
+end
