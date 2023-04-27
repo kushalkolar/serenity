cd2df552792193ad4e39f80986656c5f61ec0d32
kushalkolar <kushalkolar@gmail.com>
2023-04-27 03:38:07 -0400

    reorganize __init__

--- serenity/__init__.py
+++ serenity/__init__.py
@@ -1,3 +1,11 @@
+from warnings import warn
+
+
 from . import actors
-from .mesmerize_utils import load_batch, create_batch
-from .extensions import AcquisitionSeriesExtensions, AcquisitionDataFrameExtensions
+from . import io
+
+try:
+    from .mesmerize_utils import load_batch, create_batch
+    from .extensions import AcquisitionSeriesExtensions, AcquisitionDataFrameExtensions
+except:
+    warn("mesmerize not found, this is fine if you just need to run SerenityServer")
